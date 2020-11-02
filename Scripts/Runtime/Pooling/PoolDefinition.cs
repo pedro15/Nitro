c3ff31607ab957116ab628a036f3756f90ce7230
Pedro Duran <pedrojosuedm@hotmail.com>
2020-11-02 15:15:33 -0400

    Pool definition & demo structure

---
+++ Scripts/Runtime/Pooling/PoolDefinition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Nitro.Pooling
+{
+    [CreateAssetMenu(menuName = "Nitro/Pool Definition" , order = -10)]
+    public class PoolDefinition : ScriptableObject
+    {
+        public RecycleBinData[] poolData = default;
+    }
+}
